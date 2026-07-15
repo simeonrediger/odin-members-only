9cfb16774c413aee6890c1629d1c7c11d721de4e
Simeon Rediger <simeonrediger@gmail.com>
2026-07-15 19:24:58 -0400

    Simplify assignment of domain constants to locals

--- src/middleware/assign-locals.js
+++ src/middleware/assign-locals.js
@@ -1,26 +1,8 @@
-import {
-  MIN_USERNAME_LENGTH,
-  MAX_USERNAME_LENGTH,
-  MIN_DISPLAY_NAME_LENGTH,
-  MAX_DISPLAY_NAME_LENGTH,
-  MIN_PASSWORD_LENGTH,
-  MIN_MESSAGE_LENGTH,
-  MAX_MESSAGE_LENGTH,
-} from '../domains/constants.js';
+import * as domainConstants from '../domains/constants.js';
 
 export default function assignLocals(req, res, next) {
-  Object.assign(res.locals, locals);
+  Object.assign(res.locals, domainConstants);
   res.locals.currentUser = req.user;
   res.locals.messages = req.session.messages;
   next();
 }
-
-const locals = {
-  MIN_USERNAME_LENGTH,
-  MAX_USERNAME_LENGTH,
-  MIN_DISPLAY_NAME_LENGTH,
-  MAX_DISPLAY_NAME_LENGTH,
-  MIN_PASSWORD_LENGTH,
-  MIN_MESSAGE_LENGTH,
-  MAX_MESSAGE_LENGTH,
-};
