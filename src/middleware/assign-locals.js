349de655b2a22007cea98545b9594bfbc2935800
Simeon Rediger <simeonrediger@gmail.com>
2026-07-15 20:27:30 -0400

    Rename messages local to flashMessages

--- src/middleware/assign-locals.js
+++ src/middleware/assign-locals.js
@@ -3,6 +3,6 @@ import * as domainConstants from '../domains/constants.js';
 export default function assignLocals(req, res, next) {
   Object.assign(res.locals, domainConstants);
   res.locals.currentUser = req.user;
-  res.locals.messages = req.session.messages;
+  res.locals.flashMessages = req.session.messages;
   next();
 }
